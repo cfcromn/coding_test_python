11b99fc6de22fb7ec19b9902c6f6b7f384c0a1d4
박하민 <hamin090403@gmail.com>
2025-09-15 23:39:57 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+for i in range(num):
+    ps = input()
+    count = 0
+    for j in ps:
+        if j == '(':
+            count+=1
+        elif j == ')':
+            count-=1
+        if count < 0:
+            break
+    if count == 0:
+        print("YES")
+    else:
+        print("NO")
