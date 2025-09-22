4dd1e078a6b191db6a867970f74865f9e6b0e370
박하민 <hamin090403@gmail.com>
2025-09-22 11:24:03 +0900

    [Silver V] Title: 영화감독 숌, Time: 660 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+count = 0
+i = 0
+while True:
+    if '666' in str(i):
+        count += 1
+    if count == n:
+        print(i)
+        break
+    i += 1
