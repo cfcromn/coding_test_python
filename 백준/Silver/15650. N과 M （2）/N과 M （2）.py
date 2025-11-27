8eec535b100c39226da4fdbf2dd6e2af6ffa67b7
박하민 <hamin090403@gmail.com>
2025-11-27 09:25:39 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+def back(depth):
+    if depth == m:
+        print(*bt)
+
+    for i in range(1, n + 1):
+        if visited[i]:
+            continue
+        if bt:
+            if bt[-1] > i:
+                continue
+                
+        visited[i] = True
+        bt.append(i)
+
+        back(depth + 1)
+
+        visited[i] = False
+        bt.pop()
+
+        
+n, m = map(int, input().split())
+visited = [False] * (n + 1)
+bt = []
+back(0)
